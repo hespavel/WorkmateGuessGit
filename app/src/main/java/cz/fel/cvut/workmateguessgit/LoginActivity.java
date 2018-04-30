b445656f85f1e16819152270327e201300f91c0a
Pavel Hes <hespavel@fel.cvut.cz>
2018-04-30 09:46:22 +0200

    commit of activities

---
+++ app/src/main/java/cz/fel/cvut/workmateguessgit/LoginActivity.java
@@ -0,0 +1,27 @@
+package cz.fel.cvut.workmateguessgit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void onClickLogin(View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+
+        startActivity(intent);
+    }
+}
